c383a204bbb487fc6b687b74aa347733db55866c
Paz <parancibiac@gmail.com>
2024-08-19 21:38:00 -0400

    delete code unused

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "./componentes/Navbar";
 //import Home from "./componentes/Home";
 import Footer from "./componentes/Footer";
@@ -8,27 +8,13 @@ import Cart from "./componentes/Cart";
 //import LoginPage from "./componentes/LoginPage";
 
 function App() {
-  const [cart, setCart] = useState([]);
-  // Función para agregar un producto al carrito
-  const addCart = (product) => {
-    const searchProduct = cart.find((p) => p.id === product.id);
-    if (searchProduct) {
-      // Si el producto ya está en el carrito, solo actualizamos la cantidad
-      const updateProduct =  cart.map((p) =>
-        p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p
-      );
-      setCart(updateProduct)
-    } else {
-      setCart([...cart, { ...product, quantity: 1 }])
-    }
-  };
   return (
     <>
       <Navbar />
       {/*<Home/>*/}
       {/*<RegisterPage />*/}
       {/*<LoginPage />*/}
-      <Cart cart={cart} />
+      <Cart />
       <Footer />
     </>
   );
